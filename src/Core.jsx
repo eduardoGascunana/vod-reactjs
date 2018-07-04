0db7e7e45215b04f5bbee50b42304ee6bc1d4def
eduardoGascunana <eduardogascunana@gmail.com>
2018-07-05 01:07:53 +0200

    Core - cambios varios

--- src/Core.jsx
+++ src/Core.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-// import List from './components/list/List'
-import ViewList from './containers/list/List'
 import {
   BrowserRouter,
   Route
@@ -8,73 +6,118 @@ import {
 import { 
   Switch
 } from 'react-router'
-import DataModel from './models/data'
+import MoviesModel from './models/Movies'
+import CategoriesModel from './models/Categories'
+import styles from './common/css/styles.css'
+import ViewList from './containers/list/List'
+import Header from './components/header/Header'
 
 class Core extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      viewSelected: 'list',
+      // viewSelected: null,
       category: 'home',
-      coverList: []
-      // coverList: this.props.coverList
+      movies: [],
+      categories: []
     }
-    this.dataModel = new DataModel()
+    this.moviesModel = new MoviesModel()
+    this.categoriesModel = new CategoriesModel()
+    this.onClickMenuItem = this.onClickMenuItem.bind(this)
     this.onClickCover = this.onClickCover.bind(this)
     this.onClickIconCart = this.onClickIconCart.bind(this)
     this.onClickRating = this.onClickRating.bind(this)    
   }
   componentWillMount() {
-    this.dataModel.getItems('Action')
+    console.log("Core - componenteWillMount")
+
+    this.moviesModel.subscribe(this.render.bind(this))
+
+    this.categoriesModel.getItems()
+      .then(response => {
+        const categories = response
+        this.moviesModel.getItems('Action')
+          .then(response => {
+            const items = response.map((item) => {
+              return item
+            })
+            this.setState({
+              movies: items,
+              categories: categories
+            })
+          })
+      })
+  }
+  onClickMenuItem(index, history) {
+    console.log("Core - onClickMenuItem: ", index)
+
+    const name = this.categoriesModel.getNameCategory(index)
+    this.moviesModel.getItems(name)
       .then(response => {
         const items = response.map((item) => {
           return item
         })
         this.setState({
-          coverList: items
+          viewSelected: 'list',
+          movies: items,
+          category: name
         })
+        history.push('/list/' + name)                
       })
   }
   onClickCover(info, history) {
     console.log("Core - onClickCover: ",info)
     /* TO-DO */
     /*    https://stackoverflow.com/questions/42123261/programmatically-navigate-using-react-router-v4 */
-    // history.push('detail/' + info.nameCategory + '/' + info.id)
-    history.push('/')
+    this.setState({
+      viewSelected: 'detail'
+    })
+    history.push('detail/' + info.nameCategory + '/' + info.id)
   }
   onClickIconCart(info) {
     console.log("Core - onClickIconCart - info: ", info)
 
-    this.setState({
-      coverList: this.dataModel.setItemCart(info)
-    })
+    /* this.setState({
+      movies: this.moviesModel.setItemCart(info)
+    }) */
+    this.moviesModel.setItemCart(info)
   }
   onClickRating(info) {
     console.log("Core - onClickIconRating - info: ", info)
 
-    this.setState({
-      coverList: this.dataModel.modifyRate(info)
-    })
+    /* this.setState({
+      movies: this.moviesModel.modifyRate(info)
+    }) */
   }    
   render() {
-    console.log("Core - render")
+    if (this.state.movies[0]) {
+      console.log("Core - render ",this.state.movies[5].isAddCart)
+    }
 
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/list/:category' render={(props) => (
-            <ViewList 
-              {...props} 
-              items={this.state.coverList} 
-              handleClickCover={this.onClickCover}
-              handleClickIconCart={this.onClickIconCart}
-              handleClickRating={this.onClickRating}
-            />
-          )}/>
-          {/* <Route path='/detail/:category/:id' component={ViewDetail} data={}/> */}
-          {/* <Route path='/cart' component={ViewCart} data={}/> */}
-        </Switch>
-      </BrowserRouter>
+      <main className={styles.main} >
+        <Header 
+          viewSelected={this.state.viewSelected}/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/list/:category' render={(props) => (
+              <ViewList 
+                {...props} 
+                movies={this.state.movies} 
+                categories={this.state.categories}
+                handleClickCover={this.onClickCover}
+                handleClickIconCart={this.onClickIconCart}
+                handleClickRating={this.onClickRating}
+                handleClickMenuItem={this.onClickMenuItem}
+              />
+            )}/>
+            {/* <Route path='/detail/:category/:id' component={ViewDetail} data={}/> */}
+            {/* <Route path='/cart' component={ViewCart} data={}/> */}
+          </Switch>
+        </BrowserRouter>
+      </main>
     )
   }
 }
