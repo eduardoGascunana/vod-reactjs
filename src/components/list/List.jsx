12539c075d38a3e890af93158199afdd914aeb3a
eduardoGascunana <eduardogascunana@gmail.com>
2018-07-27 08:35:55 +0200

    List - limpieza

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,29 +6,20 @@ import Cover from '../cover/Cover'
 class List extends React.Component {
   constructor(props) {
     super(props)
-    /* this.state = {
-      moving: this.props.rate,
-      selected: this.props.rate
-    } */
     this.onClickCover = this.onClickCover.bind(this)
     this.onClickIconCart = this.onClickIconCart.bind(this)
     this.onClickRating = this.onClickRating.bind(this)
   }  
   onClickCover(info) {
-    console.log("List - onClickCover")
     this.props.handleClickCover(info)
   }
   onClickIconCart(info) {
-    console.log("List - onClickIconCart - isAdd: ", info)
     this.props.handleClickIconCart(info)
   }  
   onClickRating(info) {
-    console.log("List - onClickIconRating - rate: ", info)
     this.props.handleClickRating(info)
   }  
   render() {
-    console.log("List - render")
-
     let listCover = this.props.data.map((item, index) => {
       return <Cover 
         key={index} 
