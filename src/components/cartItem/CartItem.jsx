92d1296dff44c9385ef1d3ee4a67d4cac7f43b4e
eduardoGascunana <eduardogascunana@gmail.com>
2018-09-25 16:54:02 +0200

    CartItem - componente

---
+++ src/components/cartItem/CartItem.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './CartItem.css'
+import Icon from '../icon/Icon'
+
+class CartItem extends React.Component {
+  constructor (props) {
+    super (props)
+    this.onClickIconDelete = this.onClickIconDelete.bind(this)
+  }  
+  onClickIconDelete () {
+    if (this.props.handleClickDelete) {
+      this.props.handleClickDelete({
+        id: this.props.data.id,
+        isAdd: !this.props.data.isAddCart
+      })
+    }     
+  }
+  render () {
+    const { data } = this.props
+    let imgPath
+    try {
+      imgPath = require(`../../common/images/${data.cover}`)
+    } catch (err) {
+      imgPath = require(`../../common/images/default.jpg`)
+    }    
+    return (
+      <div className={styles.cartItem}>
+        <div className={styles.cartItemCover}>
+          <img src={imgPath} alt={data.name} />                            
+        </div>
+        <div className={styles.cartItemInfo}>
+          <div className={styles.cartItemTitle}>
+            {data.name}
+          </div>
+          <div className={styles.cartItemInfoWraperEnd}>
+            <div className={styles.cartItemPrice}>
+              {data.price}€
+              </div>
+            <div className={styles.cartItemDelete}>
+              <Icon type='delete' color='black' handleClick={this.onClickIconDelete} />
+            </div>
+          </div>
+        </div>        
+      </div>
+    )
+  }
+}
+
+CartItem.propTypes = {
+  data: PropTypes.object,
+  handleClickDelete: PropTypes.func
+}
+
+export default CartItem
