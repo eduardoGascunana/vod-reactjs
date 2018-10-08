aa1cdd8a1be9713eba5a5afc0a9937ace4efe35c
eduardoGascunana <eduardogascunana@gmail.com>
2018-10-08 15:21:58 +0200

    Modal

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Modal.css'
+
+class Modal extends React.Component {
+  constructor (props) {
+    super(props)
+    this.onClickButtons = this.onClickButtons.bind(this)
+  }
+  onClickButtons (ev) {
+    this.props.handle(ev)
+  }
+  render () {
+    const options = this.props.options.map((item, index) => {
+      return <button key={item} id={index} className={styles[this.props.class]}>{item}</button>
+    })
+
+    return (
+      <div className={styles.modal}>
+        <div className={styles.modalContent}>
+          <div className={styles.modalHeader}>
+            <div className={styles.modalHeaderTitle}>
+              {this.props.header}
+            </div>
+          </div>
+          <div className={styles.modalBody}>
+            {this.props.body}
+          </div>
+          <div className={styles.modalFooter}>
+            <div className={styles.modalFooterWraper} onClick={this.onClickButtons}>
+              <div>
+                {options}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+    )
+  }
+}
+
+Modal.defaultProps = {
+  class: 'modalButton'
+}
+
+Modal.propTypes = {
+  header: PropTypes.string,
+  body: PropTypes.string,
+  options: PropTypes.array,
+  class: PropTypes.string,
+  handle: PropTypes.func
+}
+
+export default Modal
