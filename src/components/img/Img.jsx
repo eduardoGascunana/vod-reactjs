cbe987b98515901fbf3f423faec8393ab10eee34
eduardoGascunana <eduardogascunana@gmail.com>
2018-10-08 15:20:56 +0200

    Img - ruta correcta

--- src/components/img/Img.jsx
+++ src/components/img/Img.jsx
@@ -3,14 +3,12 @@ import styles from './Img.css'
 
 class Img extends React.Component {
   render() {
-    // console.log("Img - render")
-
     const {data} = this.props
     let imgPath
     try {
-      imgPath = require(`../../images/${data.cover}`)
+      imgPath = require(`../../common/images/${data.cover}`)
     } catch (err) {
-      imgPath = require(`../../images/default.jpg`)
+      imgPath = require(`../../common/images/default.jpg`)
     }    
     return (
       <img src={imgPath} className={styles.size} alt={data.name} />
