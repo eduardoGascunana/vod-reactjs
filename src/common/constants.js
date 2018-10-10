11c12dcc4267c158a21a1d0d6d1c3c02229b1bb5
eduardoGascunana <eduardogascunana@gmail.com>
2018-10-10 11:54:48 +0200

    constants

---
+++ src/common/constants.js
@@ -0,0 +1,29 @@
+const constants = {
+  VIEW: {
+    DETAIL: 'detail',
+    CART: 'cart',
+    LIST: 'list',
+    HOME: 'home'
+  },
+  ICON: {
+    COLOR: {
+      BLACK: 'black'
+    },
+    TYPE: {
+      BACK: 'back',
+      CART: 'cart',
+      EXIT: 'exit',
+      DELETE: 'delete',
+      ADD_TO_CART: 'addToCart',
+      REMOVE_TO_CART: 'removeToCart'
+    }    
+  },
+  DATA: {
+    FOLDER: '/data/',
+    EXTENSION: '.json'
+  },
+  CATEGORY_INIT: 'Home',
+  CATEGORY_INIT_LIST: 'adventure'
+}
+
+export default constants
