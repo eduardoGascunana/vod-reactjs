a4d0bee3f765ca02fd8c8c03cf1765070e3b0072
eduardoGascunana <eduardogascunana@gmail.com>
2018-10-17 15:50:27 +0200

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-// carga el modulo de test de los componentes
-//import test from './indexTest'
-
-// carga el componente Core de la App
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Core from './Core'
-
-ReactDOM.render(< Core />, document.getElementById('root'));
